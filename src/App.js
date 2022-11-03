c7a5819d608630fb3c76569606b4e3b0c47ebec2
Ivan Ustyantsev <vangold915@gmail.com>
2022-11-03 18:18:21 +0700

    Added UseEffectComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import UseEffectComponent from './examples/useEffect';
 import UseStateComponent from './examples/useState';
 
 function App() {
   return (
     <div>
       <UseStateComponent />
+      <UseEffectComponent />
     </div>
   );
 }
---
+++ src/examples/useEffect.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UseEffectComponent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UseEffectComponent
